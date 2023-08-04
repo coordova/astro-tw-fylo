67561e41deee64f2b81417b8f7e30d833e4e19a9
coordova <grcordova@gmail.com>
2023-08-04 18:47:17 -0400

    astro-tw-fylo - 01..06/Header & Dark Mode Btn, JS for color mode -  Hero Section

---
+++ src/data/nav.js
@@ -0,0 +1,14 @@
+const navData = [
+    {
+        name: "Features",
+        path: "#features",
+        // path: "/about/",
+    },
+    {
+        name: "Testimonials",
+        path: "#testimonials",
+        // path: "/testimonials/",
+    },
+]
+
+export default navData;
